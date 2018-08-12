2e96662d77db7a4dc62a93031c6c164366cb86c2
Srinivasan Thayanithi <ibm008435@gmail.com>
2018-08-12 23:06:00 +0530

    initial Commit

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+feat <- read.table("features.txt",header = F)
+fcol <- grep("mean|std",feat$V2,ignore.case = T)
+train <- read.table(".\\train\\x_train.txt",col.names = feat$V2)
+train <- train[,fcol]
+subtrain    <- read.table(".\\train\\subject_train.txt",header = F,col.names = "sub")
+ytrain <- read.table(".\\train\\y_train.txt",col.names = "act")
+trainD <- cbind(subtrain,train,ytrain)
+test <- read.table(".\\test\\x_test.txt",col.names = feat$V2)
+subtest <- read.table(".\\test\\subject_test.txt",header = F,col.names = "sub")
+ytest <- read.table(".\\test\\y_test.txt",col.names = "act")
+test <- test[,fcol]
+testD <- cbind(subtest,test,ytest)
+mainD <- rbind(trainD,testD)
+class <- read.table("activity_label.txt")
+Final <- merge(mainD,class,by.x = "act",by.y = "V1")
+Final$act <- NULL
+names(Final) <- sub("^V2$","activity",names(Final))
+names(Final) <- gsub("\\.","",names(Final))
+library(dplyr)
+TidyMean <- Final %>% group_by(sub,activity) %>% summarise_all(mean)
+
